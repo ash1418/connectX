6648c94fa5732e0df8d2c7b503b450a99601b742
Ashwary <ashwr3012@gmail.com>
2023-04-01 16:27:35 +0530

    Added other controls and routes

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@ const express= require('express'); //same instance used as in main index.js
 const router = express.Router();
 const homeController = require('../controllers/home_controller')
 router.get('/',homeController.home);
-
+router.use('/users',require('./users'));
 module.exports = router;
