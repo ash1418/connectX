527f114f8d10e16a9c1a7fa899e7f45fc51ce4c9
Ashwary <ashwr3012@gmail.com>
2023-04-20 05:19:12 +0530

    added authentication on creating post

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,8 @@
 const express=require('express');
-
 const router= express.Router();
-
+const passport = require('passport');
 const postsController = require('../controllers/posts_controller')
-
-router.post('/create',postsController.create);
+// we still need to authenticate it because it can be possible that user knows our actions and all because it has signed in once and can create form using inspect in browser so we need to also authenticate it in router 
+router.post('/create',passport.checkAuthentication,postsController.create);
 
 module.exports = router;
