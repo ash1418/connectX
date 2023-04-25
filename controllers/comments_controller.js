a18a137d430fbe0999d6cf76e3194da4a8624251
Ashwary <ashwr3012@gmail.com>
2023-04-26 01:41:18 +0530

    deleting a post (authorized) and comment also distributed code into partials

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -25,6 +25,58 @@ module.exports.create = async function(req, res) {
     }
 
 }
+// module.exports.destroy= async function(req,res){
+//   try{
+//     const comment = await Comment.findById(req.params.id);
+//         if(comment.user==req.user.id){
+//            let postId = comment.post; //we need to first save the ifd of post before deleteing comment because we need to delete the comment from post's comments array also
+           
+//           await  Comment.deleteOne({_id : req.params.id});
+
+//           await Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id }})
+            
+//            //find postId in post and pull req.params.id's comment from comments 
+//         }
+//     }
+//     catch(err){
+//         console.log(err);
+//         return res.redirect('back');
+//     }
+//}
+module.exports.destroy = async function(req, res) {
+    try {
+      const comment = await Comment.findById(req.params.id);
+      if (comment.user == req.user.id) {
+        const postId = comment.post; //we need to first save the ifd of post before deleteing comment because we need to delete the comment from post's comments array also
+        await  Comment.deleteOne({_id : req.params.id});
+        const post = await Post.findByIdAndUpdate(
+          postId,
+          { $pull: { comments: req.params.id } }, //find postId in post and pull req.params.id's comment from comments 
+          { new: true } //If you don't use the new option when updating the post, the findByIdAndUpdate() method will return the old document instead of the updated one. This means that the post variable in the code will contain the old post document, which might not reflect the latest changes to the comments array.
+        );
+        return res.redirect('back');
+      } else {
+        return res.status(401).json({ message: 'Unauthorized' });
+      }
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ message: 'Internal Server Error' });
+    }
+  };
+
+// module.exports.destroy= function(req,res){
+//     Comment.findById(req.params.id, function(err,comment){
+//         if(comment.user==req.user.id){
+//            let postId = comment.post; //we need to first save the ifd of post before deleteing comment because we need to delete the comment from post's comments array also
+           
+//            comment.remove();
+
+//            Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id }}, function(err,post){
+//             return res.redirect('back');
+//            }) //find postId in post and pull req.params.id's comment from comments 
+//         }
+//     })
+// }
 
 
 
--- routes/comments.js
+++ routes/comments.js
@@ -4,5 +4,5 @@ const passport = require('passport');
 const commentsController = require('../controllers/comments_controller')
 // we still need to authenticate it because it can be possible that user knows our actions and all because it has signed in once and can create form using inspect in browser so we need to also authenticate it in router 
 router.post('/create',passport.checkAuthentication,commentsController.create);
-
+router.get('/destroy/:id',passport.checkAuthentication,commentsController.destroy);
 module.exports = router;
